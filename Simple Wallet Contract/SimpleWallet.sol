1af5d278c604d67517e03850f62f459951c6fee5
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-04-19 17:24:39 +0530

    fallback receive function added

---
+++ Simple Wallet Contract/SimpleWallet.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract SimpleWallet {
+
+    //fallback function to receive ether to contract
+    function receive() external payable{
+
+    }
+}
