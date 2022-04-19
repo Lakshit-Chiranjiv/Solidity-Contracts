37b3be8929ba206ea538034970085ad70d6b49fa
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-04-19 17:27:56 +0530

    added func to withdraw ether

--- Simple Wallet Contract/SimpleWallet.sol
+++ Simple Wallet Contract/SimpleWallet.sol
@@ -4,6 +4,11 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract SimpleWallet {
 
+    //function to transfer/withdraw ether from contract to a given account
+    function withdrawEther(address payable _to,uint _amount) public payable{
+        _to.transfer(_amount);
+    }
+
     //fallback function to receive ether to contract
     function receive() external payable{
 
