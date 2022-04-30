d318183ebd0a849b6ce5ee3651be0af440c3f426
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-05-01 00:20:27 +0530

    added allowance mapping and modifier

--- Simple Wallet Contract/SimpleWallet.sol
+++ Simple Wallet Contract/SimpleWallet.sol
@@ -17,8 +17,16 @@ contract SimpleWallet is Ownable {
     //     _;
     // }
 
+    mapping(address => uint) public allowance;
+
+    modifier ownerOrAllowed(_amount) {
+        //isOwner function comes from the ownable smart contract and it checks if msg.sender is owner or not
+        require(isOwner() || allowance[msg.sender] >= _amount,"Not allowed to withdraw");
+        _;
+    }
+
     //function to transfer/withdraw ether from contract to a given account
-    function withdrawEther(address payable _to,uint _amount) public payable onlyOwner{
+    function withdrawEther(address payable _to,uint _amount) public payable ownerOrAllowed(_amount){
         _to.transfer(_amount);
     }
 
