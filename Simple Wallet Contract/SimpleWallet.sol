65ac68aef1ea7f9a09ca56ff485ff49dc03c8a2a
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-05-01 00:12:05 +0530

    imported ownable conract so can remove owner logic

--- Simple Wallet Contract/SimpleWallet.sol
+++ Simple Wallet Contract/SimpleWallet.sol
@@ -2,18 +2,20 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-contract SimpleWallet {
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-    address payable public owner;
+contract SimpleWallet is Ownable {
 
-    constructor(){
-        owner = payable(msg.sender);
-    }
+    // address payable public owner;
 
-    modifier onlyOwner() {
-        require(msg.sender == owner,"Only owner can withdraw ether!!");
-        _;
-    }
+    // constructor(){
+    //     owner = payable(msg.sender);
+    // }
+
+    // modifier onlyOwner() {
+    //     require(msg.sender == owner,"Only owner can withdraw ether!!");
+    //     _;
+    // }
 
     //function to transfer/withdraw ether from contract to a given account
     function withdrawEther(address payable _to,uint _amount) public payable onlyOwner{
