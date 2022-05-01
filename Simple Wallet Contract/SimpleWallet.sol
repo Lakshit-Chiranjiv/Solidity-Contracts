f597e0854ce45af6b48671898b16a22907152142
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-05-01 08:55:41 +0530

    added safemath contract and implemented it

--- Simple Wallet Contract/SimpleWallet.sol
+++ Simple Wallet Contract/SimpleWallet.sol
@@ -3,6 +3,7 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/math/SafeMath.sol";
 
 contract Allowance is Ownable {
     // address payable public owner;
@@ -16,13 +17,15 @@ contract Allowance is Ownable {
     //     _;
     // }
 
+    using SafeMath for uint;
+
     event AllowanceChanged(address indexed _whose,address indexed _byWhom,uint _oldAllowance,uint _newAllowance);
 
     mapping(address => uint) public allowance;
 
     function addAllowance(address _who,uint _amount) public onlyOwner {
-        emit AllowanceChanged(_who,msg.sender,allowance[_who],allowance[_who]+_amount);
-        allowance[_who] += _amount;
+        emit AllowanceChanged(_who,msg.sender,allowance[_who],allowance[_who].add(_amount));
+        allowance[_who] = allowance[_who].add(_amount);
     }
 
     modifier ownerOrAllowed(_amount) {
@@ -32,8 +35,8 @@ contract Allowance is Ownable {
     }
 
     function reduceAllowance(address _who,uint _amount) internal {
-        emit AllowanceChanged(_who,msg.sender,allowance[_who],allowance[_who]-_amount);
-        allowance[_who] -= _amount;
+        emit AllowanceChanged(_who,msg.sender,allowance[_who],allowance[_who].sub(_amount));
+        allowance[_who] = allowance[_who].sub(_amount);
     }
 }
 
